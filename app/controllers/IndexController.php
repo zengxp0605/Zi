5a1939ea218ef1dd7f580c68f393b7d52cd11447
zengxp0605 <1548398984@qq.com>
2015-12-28 17:33:25 +0800

    e

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,23 +1,31 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
+
+use Zi\Config;
+use Zi\Log;
 
 /**
-* IndexController
-*/
-class IndexController extends BaseController
-{
+ * IndexController
+ */
+class IndexController extends BaseController {
 
-  public function __construct()
-  {
-	
-  }
+    public function __construct() {
+        
+    }
 
-  public function index(){
-	 // echo $this->id;
-	 echo $this->__CONTROLLER__ . '----'.$this->__ACTION__;
-	
-  }
+    public function index() {
+        Config::setConfigPath(BASE_PATH . 'config/');
+        //Config::setFileFormat('json');
+        $t = Config::get('test.tt1.tt2.tt3');
+        var_dump($t);
+        //Log::debug($t);
+        // echo $this->id;
+        echo $this->__CONTROLLER__ . '----' . $this->__ACTION__;
+    }
 
-  public function test(){
-	
-  }
+    public function test() {
+        echo  'index/test';
+    }
+
 }
---
+++ config/test.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'tt1' => array(
+        'tt2' => array(
+            'tt3' => array(
+                'tt4' => 'test444',
+            ),
+        ),
+    ),
+);
